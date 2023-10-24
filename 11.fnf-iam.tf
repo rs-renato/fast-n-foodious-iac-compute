97e83dd9f53081337eec311c558e706d266da97a
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-24 22:40:30 +0100

    chore: iam improvements

--- 11.fnf-iam.tf
+++ 11.fnf-iam.tf
@@ -1,3 +1,32 @@
+# Criando a role ECS Task Execution
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# resource "aws_iam_role" "ecsTaskRole" {
+#   name = "ecsTaskRole"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Principal = {
+#         Service = "ecs-tasks.amazonaws.com"
+#       },
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
+
 # configuracao de role IAM para o lambda
 resource "aws_iam_role" "fnf-lambda-iam-role" {
   name = "fnf-lambda-iam-role"
@@ -35,7 +64,10 @@ resource "aws_iam_policy" "fnf-lambda-iam-policy" {
           "apigateway:InvokeMethod",
           "cognito-idp:AdminInitiateAuth",
           "cognito-idp:AdminRespondToAuthChallenge",
-          "cognito-idp:AdminUserGlobalSignOut"
+          "cognito-idp:AdminUserGlobalSignOut",
+          "cognito-idp:AdminCreateUser",
+          "cognito-idp:AdminSetUserPassword",
+          "lambda:InvokeFunction"
       ],
       Resource = ["*"]
     }]
@@ -49,8 +81,67 @@ resource "aws_lambda_permission" "fnf-lambda-permission" {
   principal     = "apigateway.amazonaws.com"
 }
 
+# configuracao de permission para invocacao do lambda via cognito
+resource "aws_lambda_permission" "fnf-lambda-permission-cognito" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-authorizer.function_name
+  principal     = "cognito-idp.amazonaws.com"
+}
+
+# configuracao de permission para invocacao do lambda via api gateway
+resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+# configuracao de permission para invocacao do lambda via api gateway
+resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission-cognito" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
+  principal     = "cognito-idp.amazonaws.com"
+}
+
+# configuracao de permission para invocacao do lambda via api gateway
+resource "aws_lambda_permission" "fnf-lambda-create-user-permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-create-user.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_permission" "fnf-lambda-create-user-permission-cognito" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-create-user.function_name
+  principal     = "cognito-idp.amazonaws.com"
+}
+
+resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-pre-signup.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission-cognito" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fnf-lambda-pre-signup.function_name
+  principal     = "cognito-idp.amazonaws.com"
+}
+
 # configuracao de role vs policy
 resource "aws_iam_role_policy_attachment" "fnf-lambda-iam-policy-attachment" {
   policy_arn = aws_iam_policy.fnf-lambda-iam-policy.arn
   role = aws_iam_role.fnf-lambda-iam-role.name
+}
+
+resource "aws_iam_policy_attachment" "ecsTaskExecutionRolePolicyAttachment" {
+  name       = "AmazonECSTaskExecutionRolePolicyAttachment"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  roles      = [aws_iam_role.ecsTaskExecutionRole.name]
+}
+
+
+resource "aws_iam_policy_attachment" "ecsTaskExecutionRolePolicyAttachmentCloudWatch" {
+  name       = "AmazonECSTaskExecutionRolePolicyAttachment"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  roles      = [aws_iam_role.ecsTaskExecutionRole.name]
 }
