11ecc514159539890ba35c13e6d5552bdbea0ec1
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-24 22:02:09 +0100

    feat: provider improvements

--- 0.fnf-provider.tf
+++ 0.fnf-provider.tf
@@ -1,25 +1,16 @@
 # provider aws
 provider "aws" {
   region = "us-east-1"
-  profile = "tf-user"
 }
 
 # configuracao terraform
 terraform {
   required_version = ">=1.0"
-  
-  # backend terraform para armazenar o estado no S3
-  backend "s3" {
-    bucket  = "fnf-terraform"
-    key     = "fnf-terraform.tfstate"
-    region  = "us-east-1"
-    profile = "tf-user"
-  }
 
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 3.69.0"
+        version = "~> 5.21.0"
     }
   }
 }
