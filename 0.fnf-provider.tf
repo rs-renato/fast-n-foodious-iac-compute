44a50b624dd1ba111d3a6dfdb4b03aaeab6ba4e2
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-26 17:36:04 +0100

    feat: backend s3 to fargate

--- 0.fnf-provider.tf
+++ 0.fnf-provider.tf
@@ -1,11 +1,21 @@
 # provider aws
 provider "aws" {
   region = "us-east-1"
+  profile = "terraform"
 }
 
 # configuracao terraform
 terraform {
   required_version = ">=1.0"
+
+    # backend terraform para armazenar o estado no S3
+  backend "s3" {
+    bucket  = "fnf-terraform-fmr"
+    key     = "fnf-fargate.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    profile = "terraform" // ---> enable it just locally
+  }
 
   required_providers {
     aws = {
