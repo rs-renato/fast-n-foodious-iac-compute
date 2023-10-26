da105aafde41bbcea11ff5e7780c6df1e2e7ea97
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-26 17:34:50 +0100

    feat: moving lambda iam to other repo

--- 11.fnf-iam.tf
+++ 11.fnf-iam.tf
@@ -13,111 +13,111 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   })
 }
 
-# configuracao de role IAM para o lambda
-resource "aws_iam_role" "fnf-lambda-iam-role" {
-  name = "fnf-lambda-iam-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+# # configuracao de role IAM para o lambda
+# resource "aws_iam_role" "fnf-lambda-iam-role" {
+#   name = "fnf-lambda-iam-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
 
-# configuracao de policy IAM para o lambda
-resource "aws_iam_policy" "fnf-lambda-iam-policy" {
-  name        = "fnf-lambda-iam-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = ["arn:aws:logs:*:*:*"]
-    },
-    {
-      Effect = "Allow",
-      "Action": [
-          "apigateway:InvokeApi",
-          "apigateway:InvokeResource",
-          "apigateway:InvokeMethod",
-          "cognito-idp:AdminInitiateAuth",
-          "cognito-idp:AdminRespondToAuthChallenge",
-          "cognito-idp:AdminUserGlobalSignOut",
-          "cognito-idp:AdminCreateUser",
-          "cognito-idp:AdminSetUserPassword",
-          "lambda:InvokeFunction"
-      ],
-      Resource = ["*"]
-    }]
-  })
-}
+# # configuracao de policy IAM para o lambda
+# resource "aws_iam_policy" "fnf-lambda-iam-policy" {
+#   name        = "fnf-lambda-iam-policy"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Action = [
+#         "logs:CreateLogGroup",
+#         "logs:CreateLogStream",
+#         "logs:PutLogEvents"
+#       ]
+#       Resource = ["arn:aws:logs:*:*:*"]
+#     },
+#     {
+#       Effect = "Allow",
+#       "Action": [
+#           "apigateway:InvokeApi",
+#           "apigateway:InvokeResource",
+#           "apigateway:InvokeMethod",
+#           "cognito-idp:AdminInitiateAuth",
+#           "cognito-idp:AdminRespondToAuthChallenge",
+#           "cognito-idp:AdminUserGlobalSignOut",
+#           "cognito-idp:AdminCreateUser",
+#           "cognito-idp:AdminSetUserPassword",
+#           "lambda:InvokeFunction"
+#       ],
+#       Resource = ["*"]
+#     }]
+#   })
+# }
 
-# configuracao de permission para invocacao do lambda via api gateway
-resource "aws_lambda_permission" "fnf-lambda-permission" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
-}
+# # configuracao de permission para invocacao do lambda via api gateway
+# resource "aws_lambda_permission" "fnf-lambda-permission" {
+#   action        = "lambda:InvokeFunction"
+#   function_name =  data.terraform_remote_state.lambda_state.outputs.fnf-lambda-authorizer.function_name // aws_lambda_function.fnf-lambda-authorizer.function_name
+#   principal     = "apigateway.amazonaws.com"
+# }
 
-# configuracao de permission para invocacao do lambda via cognito
-resource "aws_lambda_permission" "fnf-lambda-permission-cognito" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-authorizer.function_name
-  principal     = "cognito-idp.amazonaws.com"
-}
+# # configuracao de permission para invocacao do lambda via cognito
+# resource "aws_lambda_permission" "fnf-lambda-permission-cognito" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-authorizer.function_name // aws_lambda_function.fnf-lambda-authorizer.function_name
+#   principal     = "cognito-idp.amazonaws.com"
+# }
 
-# configuracao de permission para invocacao do lambda via api gateway
-resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
-}
+# # configuracao de permission para invocacao do lambda via api gateway
+# resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission" {
+#   action        = "lambda:InvokeFunction"
+#   function_name =  data.terraform_remote_state.lambda_state.outputs.fnf-lambda-pre-token-authorizer.function_name // aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
+#   principal     = "apigateway.amazonaws.com"
+# }
 
-# configuracao de permission para invocacao do lambda via api gateway
-resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission-cognito" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
-  principal     = "cognito-idp.amazonaws.com"
-}
+# # configuracao de permission para invocacao do lambda via api gateway
+# resource "aws_lambda_permission" "fnf-lambda-pre-auth-permission-cognito" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-pre-token-authorizer.function_name // aws_lambda_function.fnf-lambda-pre-token-authorizer.function_name
+#   principal     = "cognito-idp.amazonaws.com"
+# }
 
-# configuracao de permission para invocacao do lambda via api gateway
-resource "aws_lambda_permission" "fnf-lambda-create-user-permission" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-create-user.function_name
-  principal     = "apigateway.amazonaws.com"
-}
+# # configuracao de permission para invocacao do lambda via api gateway
+# resource "aws_lambda_permission" "fnf-lambda-create-user-permission" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-create-user.function_name // aws_lambda_function.fnf-lambda-create-user.function_name
+#   principal     = "apigateway.amazonaws.com"
+# }
 
-resource "aws_lambda_permission" "fnf-lambda-create-user-permission-cognito" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-create-user.function_name
-  principal     = "cognito-idp.amazonaws.com"
-}
+# resource "aws_lambda_permission" "fnf-lambda-create-user-permission-cognito" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-create-user.function_name // aws_lambda_function.fnf-lambda-create-user.function_name
+#   principal     = "cognito-idp.amazonaws.com"
+# }
 
-resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-pre-signup.function_name
-  principal     = "apigateway.amazonaws.com"
-}
+# resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-pre-signup.function_name // aws_lambda_function.fnf-lambda-pre-signup.function_name
+#   principal     = "apigateway.amazonaws.com"
+# }
 
-resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission-cognito" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fnf-lambda-pre-signup.function_name
-  principal     = "cognito-idp.amazonaws.com"
-}
+# resource "aws_lambda_permission" "fnf-lambda-pre-signup-permission-cognito" {
+#   action        = "lambda:InvokeFunction"
+#   function_name = data.terraform_remote_state.lambda_state.outputs.fnf-lambda-pre-signup.function_name // aws_lambda_function.fnf-lambda-pre-signup.function_name
+#   principal     = "cognito-idp.amazonaws.com"
+# }
 
-# configuracao de role vs policy
-resource "aws_iam_role_policy_attachment" "fnf-lambda-iam-policy-attachment" {
-  policy_arn = aws_iam_policy.fnf-lambda-iam-policy.arn
-  role = aws_iam_role.fnf-lambda-iam-role.name
-}
+# # configuracao de role vs policy
+# resource "aws_iam_role_policy_attachment" "fnf-lambda-iam-policy-attachment" {
+#   policy_arn = aws_iam_policy.fnf-lambda-iam-policy.arn
+#   role = aws_iam_role.fnf-lambda-iam-role.name
+# }
 
 resource "aws_iam_policy_attachment" "ecsTaskExecutionRolePolicyAttachment" {
   name       = "AmazonECSTaskExecutionRolePolicyAttachment"
