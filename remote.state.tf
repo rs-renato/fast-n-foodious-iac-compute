d6faa42c2e7cccd582f446c1602d22ea37af8794
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-28 23:37:56 +0100

    chore: remote state update

--- remote.state.tf
+++ remote.state.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "network" {
     region         = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "storage" {
+  backend = "s3"
+  config = {
+    bucket         = "fnf-terraform-storage"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
