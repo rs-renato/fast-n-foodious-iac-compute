948380a1944a8a3b4038b0c961954073b8bad4e1
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-28 15:39:21 +0100

    chore: removing s3 state

--- main.tf
+++ main.tf
@@ -7,16 +7,6 @@ provider "aws" {
 # configuracao terraform
 terraform {
   required_version = ">=1.0"
-
-    # backend terraform para armazenar o estado no S3
-  backend "s3" {
-    bucket  = "fnf-terraform-fmr"
-    key     = "fnf-fargate.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-    profile = "terraform" // ---> enable it just locally
-  }
-
   required_providers {
     aws = {
         source = "hashicorp/aws"
