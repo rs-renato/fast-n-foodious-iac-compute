c5da20be519e7a25a285a06d524e1c4d63b07fb0
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-28 23:54:07 +0100

    chore: backend s3

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 # provider aws
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform"
 }
 
 # configuracao terraform
 terraform {
   required_version = ">=1.0"
+
+  backend "s3" {
+    bucket  = "fnf-terraform-compute"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+  }
+
   required_providers {
     aws = {
         source = "hashicorp/aws"
