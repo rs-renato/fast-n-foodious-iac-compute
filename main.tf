46885eb6ad97bbe701a21fa7301f9bcd2dc77e44
Renato Rodrigues <spamcares-github@yahoo.com>
2023-10-30 21:47:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">=1.0"
 
   backend "s3" {
-    bucket  = "fnf-terraform-compute"
+    bucket  = "fast-n-foodious-terraform-compute"
     key     = "terraform.tfstate"
     region  = "us-east-1"
   }
