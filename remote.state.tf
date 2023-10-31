a19a6c517e4ab77e4ff0cfb5ceb76f782cb06437
Renato Rodrigues <spamcares-github@yahoo.com>
2023-10-30 21:47:35 -0300

    Update remote.state.tf

--- remote.state.tf
+++ remote.state.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket         = "fnf-terraform-network"
+    bucket         = "fast-n-foodious-terraform-network"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
@@ -10,7 +10,7 @@ data "terraform_remote_state" "network" {
 data "terraform_remote_state" "storage" {
   backend = "s3"
   config = {
-    bucket         = "fnf-terraform-storage"
+    bucket         = "fast-n-foodious-terraform-storage"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
